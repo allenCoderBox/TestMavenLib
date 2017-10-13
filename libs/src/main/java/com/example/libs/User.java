7df9849bcc7ef86421da8b9baf51f10c50bd44a7
husongzhen <husongzhen@MacBook-Pro.local>
2017-10-13 15:56:41 +0800

    fix maven

---
+++ libs/src/main/java/com/example/libs/User.java
@@ -0,0 +1,17 @@
+package com.example.libs;
+
+/**
+ * Created by husongzhen on 17/10/13.
+ */
+
+public class User {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
