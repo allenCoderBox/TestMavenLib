38aed42ac13b4b892aecfed9f1d639f07d767806
husongzhen <husongzhen@MacBook-Pro.local>
2017-10-13 16:11:57 +0800

    fxi

--- app/src/main/java/com/example/husongzhen/testmaven/MainActivity.java
+++ app/src/main/java/com/example/husongzhen/testmaven/MainActivity.java
@@ -3,11 +3,17 @@ package com.example.husongzhen.testmaven;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.libs.User;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        User user = new User();
+        user.setName("usobgzhes");
     }
 }
---
+++ libs2/src/main/java/com/example/libs2/Age.java
@@ -0,0 +1,10 @@
+package com.example.libs2;
+
+/**
+ * Created by husongzhen on 17/10/13.
+ */
+
+public class Age {
+
+
+}
